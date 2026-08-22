4abffe0389e7cd2dcd05cc9730e58fe1d161187d
HariisDermawan <153626741+HariisDermawan@users.noreply.github.com>
2026-08-23 01:45:00 +0700

    Add Product Category management with CRUD operations and validation

---
+++ app/Http/Controllers/Api/V1/ProductCategoryController.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1;
+
+use App\Helpers\ApiResponse;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\ProductCategoryRequest;
+use App\Http\Resources\PaginatedResource;
+use App\Http\Resources\ProductCategoryResource;
+use App\Models\ProductCategory;
+use Illuminate\Http\Request;
+
+class ProductCategoryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(ProductCategoryRequest $request)
+    {
+        $categories = ProductCategory::search($request->search)
+            ->latest()
+            ->paginate($request->limit ?? 10);
+        return ApiResponse::success(
+            new PaginatedResource($categories, ProductCategoryResource::class),
+            'Product Categories list'
+        );
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,14 +1,15 @@
 <?php
 
 use App\Http\Controllers\Api\V1\AuthController;
+use App\Http\Controllers\Api\V1\ProductCategoryController;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('v1')->group( function () {
     Route::POST('/login', [AuthController::class, 'login']);
-
-
     Route::middleware('auth:sanctum')->group(function() {
         Route::GET('/me', [AuthController::class, 'me']);
         Route::POST('/logout', [AuthController::class, 'logout']);
+
+        Route::apiResource('product-categories', ProductCategoryController::class);
     });
 });
